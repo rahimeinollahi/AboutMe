43a6a5d75d012b8ed2e510537e5f6b316fcf5107
rahim einollahi <r.einollahi@gamil.com>
2022-05-07 18:38:05 +0430

    scss folder add to src folder

---
+++ src/components/HomeComponents/MainNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>در اینده اضافه خواهم کرد</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"MainNavbar"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
